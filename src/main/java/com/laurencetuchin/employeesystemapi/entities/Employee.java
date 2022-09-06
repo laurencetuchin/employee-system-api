3e98e638cb1cdf6a76774dc67f654040ecaf7d5f
laurencetuchin <laurencetuchin@gmail.com>
2022-09-06 16:45:37 +1000

    Create employee entity

---
+++ src/main/java/com/laurencetuchin/employeesystemapi/entities/Employee.java
@@ -0,0 +1,17 @@
+package com.laurencetuchin.employeesystemapi.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue
+    Long id;
+
+
+}
