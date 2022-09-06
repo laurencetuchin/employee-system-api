1d5c10aa908630e2f2f920405d583202f67ef04b
laurencetuchin <laurencetuchin@gmail.com>
2022-09-06 16:35:59 +1000

    Initial commit

---
+++ src/test/java/com/laurencetuchin/employeesystemapi/EmployeeSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laurencetuchin.employeesystemapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
