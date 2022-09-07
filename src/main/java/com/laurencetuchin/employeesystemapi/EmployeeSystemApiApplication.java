d69d5228d5da2e4941a5f15d9a9270d7a9520869
laurencetuchin <laurencetuchin@gmail.com>
2022-09-07 18:53:51 +1000

    Add seed data

--- src/main/java/com/laurencetuchin/employeesystemapi/EmployeeSystemApiApplication.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/EmployeeSystemApiApplication.java
@@ -1,8 +1,11 @@
 package com.laurencetuchin.employeesystemapi;
 
+import com.laurencetuchin.employeesystemapi.entities.Employee;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class EmployeeSystemApiApplication {
@@ -11,4 +14,12 @@ public class EmployeeSystemApiApplication {
 		SpringApplication.run(EmployeeSystemApiApplication.class, args);
 	}
 
+//	@Bean
+//	public CommandLineRunner loadDatabase(EmployeeRepository employeeRepository){
+//		return args -> {
+//			employeeRepository.save(new Employee(""));
+//			employeeRepository.save(new Employee(""));
+//			employeeRepository.save(new Employee(""));
+//		}
+//	}
 }
