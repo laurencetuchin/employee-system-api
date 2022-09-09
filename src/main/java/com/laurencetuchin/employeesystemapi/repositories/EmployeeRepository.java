857afc5711ffd32af100f7b2a047729d62c32eda
laurencetuchin <laurencetuchin@gmail.com>
2022-09-09 11:40:00 +1000

    remove CrudRepository from interface

--- src/main/java/com/laurencetuchin/employeesystemapi/repositories/EmployeeRepository.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/repositories/EmployeeRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EmployeeRepository extends JpaRepository<Employee, Long>, CrudRepository<Employee, Long> {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
