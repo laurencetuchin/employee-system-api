5a21abd6ca811dd6a0324707c2866d3a980e2b0a
laurencetuchin <laurencetuchin@gmail.com>
2022-09-09 13:39:49 +1000

    Add EmployeeService

---
+++ src/main/java/com/laurencetuchin/employeesystemapi/services/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.laurencetuchin.employeesystemapi.services;
+
+import com.laurencetuchin.employeesystemapi.entities.Employee;
+import com.laurencetuchin.employeesystemapi.repositories.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+
+
+}
