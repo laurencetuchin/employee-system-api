92b174094237d94560fd5f8782d46c46b8066b3d
laurencetuchin <laurencetuchin@gmail.com>
2022-09-09 11:33:44 +1000

    Create seed data class

---
+++ src/main/java/com/laurencetuchin/employeesystemapi/DataLoader.java
@@ -0,0 +1,18 @@
+package com.laurencetuchin.employeesystemapi;
+
+import com.laurencetuchin.employeesystemapi.entities.Employee;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader {
+
+    @Bean
+    CommandLineRunner loadData(EmployeeRepository repository){
+        return args -> {
+            repository.save(new Employee("Frodo", "Ring bearer"));
+            repository.save(new Employee("Bilbo", "Ring carer"));
+        }
+    }
+}
