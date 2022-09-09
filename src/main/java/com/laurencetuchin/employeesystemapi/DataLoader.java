a860997ffee01b3b1d3644911f0c2f1264254507
laurencetuchin <laurencetuchin@gmail.com>
2022-09-09 11:37:23 +1000

    Add seed data

--- src/main/java/com/laurencetuchin/employeesystemapi/DataLoader.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/DataLoader.java
@@ -1,6 +1,7 @@
 package com.laurencetuchin.employeesystemapi;
 
 import com.laurencetuchin.employeesystemapi.entities.Employee;
+import com.laurencetuchin.employeesystemapi.repositories.EmployeeRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
@@ -13,6 +14,7 @@ public class DataLoader {
         return args -> {
             repository.save(new Employee("Frodo", "Ring bearer"));
             repository.save(new Employee("Bilbo", "Ring carer"));
-        }
+            repository.save(new Employee("Samwise", "helper"));
+        };
     }
 }
