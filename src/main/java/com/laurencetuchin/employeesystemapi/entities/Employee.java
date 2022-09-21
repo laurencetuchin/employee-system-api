31a8b057ddb81be587c8b8dc2a31b8385619645d
laurencetuchin <laurencetuchin@gmail.com>
2022-09-21 16:03:04 +1000

    Update Entity column names

--- src/main/java/com/laurencetuchin/employeesystemapi/entities/Employee.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/entities/Employee.java
@@ -1,9 +1,6 @@
 package com.laurencetuchin.employeesystemapi.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "employees")
@@ -13,10 +10,13 @@ public class Employee {
     @GeneratedValue
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    @Column(name = "role")
     private String role;
 
+    @Column(name = "employed")
     private boolean isCurrentlyWorkingAtCompany;
 
 
