3469f0648f2b37856af35de1efe70ad1c459c2f5
laurencetuchin <laurencetuchin@gmail.com>
2022-10-18 15:34:02 +1100

    Add log info to DataLoader

--- src/main/java/com/laurencetuchin/employeesystemapi/seedData/DataLoader.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/seedData/DataLoader.java
@@ -2,6 +2,8 @@ package com.laurencetuchin.employeesystemapi.seedData;
 
 import com.laurencetuchin.employeesystemapi.entities.Employee;
 import com.laurencetuchin.employeesystemapi.repositories.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +13,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class DataLoader {
 
+    private static final Logger log = LoggerFactory.getLogger(DataLoader.class);
+
     @Autowired
     private EmployeeRepository repository;
 
@@ -18,8 +22,11 @@ public class DataLoader {
     CommandLineRunner loadData(EmployeeRepository repository){
         return args -> {
             repository.save(new Employee("Frodo","Ring bearer"));
+            log.info("Employee 1 saved in Database");
             repository.save(new Employee("Bilbo","Ring bearer"));
+            log.info("Employee 2 saved in Database");
             repository.save(new Employee("samwise","ring friend"));
+            log.info(" 3 Employees saved in Database");
 
         };
     }
