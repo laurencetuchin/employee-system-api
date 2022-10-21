a1b5f93d88831a017000803782315ab954356b57
laurencetuchin <laurencetuchin@gmail.com>
2022-10-21 15:39:07 +1100

    Add ErrorDTO for client

--- src/main/java/com/laurencetuchin/employeesystemapi/dto/ErrorDTO.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/dto/ErrorDTO.java
@@ -1,7 +1,116 @@
 package com.laurencetuchin.employeesystemapi.dto;
 
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class ErrorDTO {
 
     public String title; // shows briefly what error is
+    public String detail;
+    public int status;
+    public String errorType;
+    public String errorCode;
+
+    public String timestamp = ZonedDateTime.now().format(DateTimeFormatter.ofPattern("yyy-MM-dd hh:mm:ss a z Z"));
+
+
+    public ErrorDTO(String title, String detail, int status, String errorType, String errorCode) {
+        this.title = title;
+        this.detail = detail;
+        this.status = status;
+        this.errorType = errorType;
+        this.errorCode = errorCode;
+    }
+
+    public static ErrorDTOBuilder builder(){
+        return new ErrorDTOBuilder();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getErrorType() {
+        return errorType;
+    }
+
+    public void setErrorType(String errorType) {
+        this.errorType = errorType;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public class ErrorDTOBuilder {
+        public String title;
+        public String detail;
+        public int status;
+        public String errorType;
+        public String errorCode;
+
+        public ErrorDTOBuilder withTitle(final String title) {
+            this.title = title;
+            return this;
+        }
+
+        public ErrorDTOBuilder withDetail(final String detail) {
+            this.detail = detail;
+            return this;
+        }
+
+        public ErrorDTOBuilder withStatus(final int status) {
+            this.status = status;
+            return this;
+        }
+
+        public ErrorDTOBuilder withErrorType(final String errorType){
+            this.errorType = errorType;
+            return this;
+        }
+
+        public ErrorDTOBuilder withErrorCode(final String errorCode){
+            this.errorCode = errorCode;
+            return this;
+        }
+
+        public ErrorDTO build() {
+            return new ErrorDTO(title, detail, status, errorType, errorCode);
+        }
+
+
+    }
 
 }
--- src/main/java/com/laurencetuchin/employeesystemapi/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,23 @@
 package com.laurencetuchin.employeesystemapi.exceptions;
 
+import com.laurencetuchin.employeesystemapi.dto.ErrorDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    public ResponseEntity<Object> handleEmployeeNotFoundException(final EmployeeNotFoundException exception){
-        return new ResponseEntity<Object>();
+    @ExceptionHandler(EmployeeNotFoundException.class)
+    public ResponseEntity<ErrorDTO> handleEmployeeNotFoundException(final EmployeeNotFoundException exception) {
+        return new ResponseEntity<ErrorDTO>(
+                ErrorDTO.builder()
+                        .withTitle("Invalid Employee")
+                        .withDetail(exception.getMessage())
+                        .withStatus(HttpStatus.NOT_FOUND.value())
+                        .withErrorType(EmployeeNotFoundException.class.getSimpleName())
+                        .withErrorCode("Custom Error Code XXX").build(),
+                HttpStatus.NOT_FOUND);
     }
 }
