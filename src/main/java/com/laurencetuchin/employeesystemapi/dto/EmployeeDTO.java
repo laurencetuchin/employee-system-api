19301dbf38f315d5ffe0087d2070404ffbb1fe36
laurencetuchin <laurencetuchin@gmail.com>
2022-10-24 11:41:11 +1100

    Add EmployeeDTO with @Component removed

--- src/main/java/com/laurencetuchin/employeesystemapi/dto/EmployeeDTO.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/dto/EmployeeDTO.java
@@ -1,31 +1,22 @@
 package com.laurencetuchin.employeesystemapi.dto;
 
 import com.laurencetuchin.employeesystemapi.entities.Employee;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
-//@Component
-//public class EmployeeDTO {
-//
-//    private Employee employee;
-//
-////    public EmployeeDTO() {
-////    }
-//
-//    @Autowired
-//    public EmployeeDTO(Employee employee) {
-//        this.employee = employee;
-//    }
-//
-//    public Long getEmployeeId() {
-//        return this.employee.getId();
-//    }
-//
-//    public String getEmployeeName() {
-//        return this.employee.getName();
-//    }
-//
-//
-//}
+public class EmployeeDTO {
+
+    private Employee employee;
+
+    public EmployeeDTO(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Long getEmployeeId() {
+        return this.employee.getId();
+    }
+
+    public String getEmployeeName() {
+        return this.employee.getName();
+    }
+
+
+}
