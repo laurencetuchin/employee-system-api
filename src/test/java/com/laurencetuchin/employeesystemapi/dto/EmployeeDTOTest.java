8d0b2e22db8af62914795c63f69c394deab30cd7
laurencetuchin <laurencetuchin@gmail.com>
2022-10-24 14:49:08 +1100

    Add EmployeeDTOTest class

---
+++ src/test/java/com/laurencetuchin/employeesystemapi/dto/EmployeeDTOTest.java
@@ -0,0 +1,21 @@
+package com.laurencetuchin.employeesystemapi.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeDTOTest {
+
+    @Test
+    void getEmployeeName() {
+    }
+
+    @Test
+    void getEmployeeRole() {
+        // assert
+
+        // when
+
+        // then
+    }
+}
