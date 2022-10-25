721cfc28a8437231aecff25e6b31bc4d94b035a9
laurencetuchin <laurencetuchin@gmail.com>
2022-10-25 17:01:55 +1100

    Add tests for Employee

--- src/test/java/com/laurencetuchin/employeesystemapi/dto/EmployeeDTOTest.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/dto/EmployeeDTOTest.java
@@ -1,10 +1,15 @@
 package com.laurencetuchin.employeesystemapi.dto;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeDTOTest {
+
+//    private final EmployeeDTO employeeDTO = new EmployeeDTO();
+
+    @Autowired EmployeeDTO employeeDTO;
 
     @Test
     void getEmployeeName() {
@@ -12,7 +17,7 @@ class EmployeeDTOTest {
 
     @Test
     void getEmployeeRole() {
-        // assert
+        // given
 
         // when
 
---
+++ src/test/java/com/laurencetuchin/employeesystemapi/repositories/EmployeeRepositoryTest.java
@@ -0,0 +1,26 @@
+package com.laurencetuchin.employeesystemapi.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeRepositoryTest {
+
+    @Test
+    void findByNameIgnoreCaseContains() {
+    }
+
+    @Test
+    void findByRoleIgnoreCaseContains() {
+    }
+
+    @Test
+    void findByIsCurrentlyWorkingAtCompany() {
+    }
+
+    @Test
+    void findEmployeeByNameAndRole() {
+    }
+
+
+}
