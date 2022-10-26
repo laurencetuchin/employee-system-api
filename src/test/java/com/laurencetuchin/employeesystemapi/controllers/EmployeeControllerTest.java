407d7541e54014eead22637b5a893b995b3ec168
laurencetuchin <laurencetuchin@gmail.com>
2022-10-26 15:22:43 +1100

    Add test for EmployeeController

---
+++ src/test/java/com/laurencetuchin/employeesystemapi/controllers/EmployeeControllerTest.java
@@ -0,0 +1,48 @@
+package com.laurencetuchin.employeesystemapi.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+
+    @Test
+    void getEmployeeById() {
+    }
+
+    @Test
+    void getAllEmployees() {
+    }
+
+    @Test
+    void getAllEmployeesDTO() {
+    }
+
+    @Test
+    void getCurrentlyEmployedEmployees() {
+    }
+
+    @Test
+    void findByNameIgnoreCaseContains() {
+    }
+
+    @Test
+    void findByRoleIgnoreCaseContains() {
+    }
+
+    @Test
+    void findEmployeeByNameAndRole() {
+    }
+
+    @Test
+    void createEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+}
--- src/test/java/com/laurencetuchin/employeesystemapi/repositories/EmployeeRepositoryTest.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/repositories/EmployeeRepositoryTest.java
@@ -1,25 +1,48 @@
 package com.laurencetuchin.employeesystemapi.repositories;
 
+import com.laurencetuchin.employeesystemapi.entities.Employee;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeRepositoryTest {
 
+
+    @Autowired
+    private EmployeeRepository underTest;
+
     @Test
-    void findByNameIgnoreCaseContains() {
+    void itShouldFindByNameIgnoreCaseContains() {
+
+        // given
+        Employee employee = new Employee(
+                "Tom",
+                "Destroying Batman",
+                true
+        );
+        underTest.save(employee);
+        // when
+        List<Employee> correctName = underTest.findByNameIgnoreCaseContains("Tom");
+
+        // then
+        assertThat(correctName).isEqualTo(employee.getName());
     }
 
     @Test
-    void findByRoleIgnoreCaseContains() {
+    void itShouldFindByRoleIgnoreCaseContains() {
     }
 
     @Test
-    void findByIsCurrentlyWorkingAtCompany() {
+    void itShouldFindByIsCurrentlyWorkingAtCompany() {
     }
 
     @Test
-    void findEmployeeByNameAndRole() {
+    void itShouldFindEmployeeByNameAndRole() {
     }
 
 
