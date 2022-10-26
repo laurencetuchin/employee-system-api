7d9ff92b75279dd10425a8a3f0ddac81f645b880
laurencetuchin <laurencetuchin@gmail.com>
2022-10-26 15:26:36 +1100

    Add @WebMvcTest to class

--- src/test/java/com/laurencetuchin/employeesystemapi/controllers/EmployeeControllerTest.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/controllers/EmployeeControllerTest.java
@@ -3,9 +3,11 @@ package com.laurencetuchin.employeesystemapi.controllers;
 import com.laurencetuchin.employeesystemapi.services.EmployeeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest
 class EmployeeControllerTest {
 
 
