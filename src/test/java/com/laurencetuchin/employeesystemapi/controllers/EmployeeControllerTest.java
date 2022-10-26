4463330ec9f71511c32c58e577fe716f4d485c55
laurencetuchin <laurencetuchin@gmail.com>
2022-10-26 15:28:29 +1100

    Update @WebMvcTest to include EmployeeController.class

--- src/test/java/com/laurencetuchin/employeesystemapi/controllers/EmployeeControllerTest.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/controllers/EmployeeControllerTest.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@WebMvcTest
+@WebMvcTest(EmployeeController.class)
 class EmployeeControllerTest {
 
 
