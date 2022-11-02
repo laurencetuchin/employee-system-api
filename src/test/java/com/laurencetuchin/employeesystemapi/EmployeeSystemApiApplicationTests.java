bdf8d7134377ee97aaeed44530a1fcbc96b4de3a
laurencetuchin <laurencetuchin@gmail.com>
2022-11-02 15:27:51 +1100

    Add EmployeeController check for app context

--- src/test/java/com/laurencetuchin/employeesystemapi/EmployeeSystemApiApplicationTests.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/EmployeeSystemApiApplicationTests.java
@@ -1,13 +1,21 @@
 package com.laurencetuchin.employeesystemapi;
 
+import com.laurencetuchin.employeesystemapi.controllers.EmployeeController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class EmployeeSystemApiApplicationTests {
 
+	@Autowired
+	private EmployeeController controller;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+		assertThat(controller).isNotNull();
 	}
 
 }
