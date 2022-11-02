474581d3c58b5741d73317fca5e6cfeeee736e27
laurencetuchin <laurencetuchin@gmail.com>
2022-11-02 17:19:49 +1100

    Add HttpRequestTest class

---
+++ src/test/java/com/laurencetuchin/employeesystemapi/HttpRequestTest.java
@@ -0,0 +1,9 @@
+package com.laurencetuchin.employeesystemapi;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HttpRequestTest {
+
+
+}
