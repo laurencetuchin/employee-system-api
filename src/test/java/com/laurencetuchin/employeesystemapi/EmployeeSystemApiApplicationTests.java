31c4c57a4310aec155b84fd61c508b25fd6b6f78
laurencetuchin <laurencetuchin@gmail.com>
2022-11-02 16:05:49 +1100

    Add employee Service to app context

--- src/test/java/com/laurencetuchin/employeesystemapi/EmployeeSystemApiApplicationTests.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/EmployeeSystemApiApplicationTests.java
@@ -1,6 +1,7 @@
 package com.laurencetuchin.employeesystemapi;
 
 import com.laurencetuchin.employeesystemapi.controllers.EmployeeController;
+import com.laurencetuchin.employeesystemapi.services.EmployeeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,9 +14,13 @@ class EmployeeSystemApiApplicationTests {
 	@Autowired
 	private EmployeeController controller;
 
+	@Autowired
+	private EmployeeService employeeService;
+
 	@Test
 	void contextLoads() throws Exception {
 		assertThat(controller).isNotNull();
+		assertThat(employeeService).isNotNull();
 	}
 
 }
