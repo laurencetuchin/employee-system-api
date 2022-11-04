73c482f77b3f9de7cc61be8c628ba61ab8971c90
laurencetuchin <laurencetuchin@gmail.com>
2022-11-04 15:11:27 +1100

    Optimise imports for test

--- src/test/java/com/laurencetuchin/employeesystemapi/controllers/EmployeeControllerTest.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/controllers/EmployeeControllerTest.java
@@ -2,21 +2,18 @@ package com.laurencetuchin.employeesystemapi.controllers;
 
 import com.laurencetuchin.employeesystemapi.entities.Employee;
 import com.laurencetuchin.employeesystemapi.services.EmployeeService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.provider.NullSource;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @WebMvcTest(EmployeeController.class)
 class EmployeeControllerTest {
@@ -46,7 +43,14 @@ class EmployeeControllerTest {
 
     @Test
     void getAllEmployees() {
+        List<Employee> allEmployees = new ArrayList<>();
+        allEmployees.add(new Employee("Frodo Baggins","Ring bearer",true));
+        allEmployees.add(new Employee("Bilbo Baggins", "Ring thief",true));
+        allEmployees.add(new Employee("Peter Potatohead", "Office admin",false));
 
+        Mockito.when(employeeService.getAllEmployees()).thenReturn(allEmployees);
+        String url = "/api/employees/all";
+        mockMvc.perform(MockHttpServletRequest::new(url)).andExpect
     }
 
     @Test
