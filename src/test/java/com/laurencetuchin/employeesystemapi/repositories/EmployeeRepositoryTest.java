642077dbc6ede52e386ba495074f3953635780cc
laurencetuchin <laurencetuchin@gmail.com>
2022-11-07 11:35:58 +1100

    Add @DataJpaTest to test class

--- src/test/java/com/laurencetuchin/employeesystemapi/repositories/EmployeeRepositoryTest.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/repositories/EmployeeRepositoryTest.java
@@ -3,11 +3,13 @@ package com.laurencetuchin.employeesystemapi.repositories;
 import com.laurencetuchin.employeesystemapi.entities.Employee;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@DataJpaTest
 class EmployeeRepositoryTest {
 
 
