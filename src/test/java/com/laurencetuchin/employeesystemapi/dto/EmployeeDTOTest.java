c58b9a69046ec2c95376737e27dad0babe26589e
laurencetuchin <laurencetuchin@gmail.com>
2022-11-10 10:51:09 +1100

    Add @Autowire EmployeeDTO and @MockBean EmployeeService

--- src/test/java/com/laurencetuchin/employeesystemapi/dto/EmployeeDTOTest.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/dto/EmployeeDTOTest.java
@@ -1,7 +1,9 @@
 package com.laurencetuchin.employeesystemapi.dto;
 
+import com.laurencetuchin.employeesystemapi.services.EmployeeService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,10 +11,15 @@ class EmployeeDTOTest {
 
 //    private final EmployeeDTO employeeDTO = new EmployeeDTO();
 
-    @Autowired EmployeeDTO employeeDTO;
+    @Autowired
+    private EmployeeDTO employeeDTO;
+
+    @MockBean
+    private EmployeeService employeeService;
 
     @Test
     void getEmployeeName() {
+
     }
 
     @Test
