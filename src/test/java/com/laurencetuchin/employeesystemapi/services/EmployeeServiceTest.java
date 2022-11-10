4abcc0f285890eeee73115788d1c089a6c815c9d
laurencetuchin <laurencetuchin@gmail.com>
2022-11-10 14:24:09 +1100

    Add test class for Employee Service

---
+++ src/test/java/com/laurencetuchin/employeesystemapi/services/EmployeeServiceTest.java
@@ -0,0 +1,60 @@
+package com.laurencetuchin.employeesystemapi.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceTest {
+
+    @Test
+    void findByNameIgnoreCaseContains() {
+    }
+
+    @Test
+    void findByRoleIgnoreCaseContains() {
+    }
+
+    @Test
+    void findCurrentlyEmployedEmployees() {
+    }
+
+    @Test
+    void findEmployeeByNameAndRole() {
+    }
+
+    @Test
+    void findEmployeeById() {
+    }
+
+    @Test
+    void getAllEmployees() {
+    }
+
+    @Test
+    void getAllEmployeesDTO() {
+    }
+
+    @Test
+    void getEmployeeDTOById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void getEmployeeById() {
+    }
+
+    @Test
+    void addNewEmployee() {
+    }
+
+    @Test
+    void deleteEmployeeById() {
+    }
+
+    @Test
+    void updateEmployeeById() {
+    }
+}
