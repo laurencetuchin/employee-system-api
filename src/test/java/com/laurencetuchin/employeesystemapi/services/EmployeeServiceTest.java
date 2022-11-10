8e7162e59a30214580b6268214768a8198989856
laurencetuchin <laurencetuchin@gmail.com>
2022-11-10 14:25:33 +1100

    Add in @SpringBootTest to class

--- src/test/java/com/laurencetuchin/employeesystemapi/services/EmployeeServiceTest.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/services/EmployeeServiceTest.java
@@ -1,9 +1,11 @@
 package com.laurencetuchin.employeesystemapi.services;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class EmployeeServiceTest {
 
     @Test
