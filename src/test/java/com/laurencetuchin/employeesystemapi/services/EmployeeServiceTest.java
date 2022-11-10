3db9ac23d6cfd6e44d2abd5655dd0663f55f95c2
laurencetuchin <laurencetuchin@gmail.com>
2022-11-10 14:26:21 +1100

    Add in @Autowired EmployeeService

--- src/test/java/com/laurencetuchin/employeesystemapi/services/EmployeeServiceTest.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/services/EmployeeServiceTest.java
@@ -1,12 +1,16 @@
 package com.laurencetuchin.employeesystemapi.services;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class EmployeeServiceTest {
+
+    @Autowired
+    private EmployeeService employeeService;
 
     @Test
     void findByNameIgnoreCaseContains() {
