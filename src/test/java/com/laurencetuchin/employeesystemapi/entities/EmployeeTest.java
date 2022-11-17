a7ceac851a4f580d082f7b219b8f1d607dbbbf69
laurencetuchin <laurencetuchin@gmail.com>
2022-11-17 11:28:06 +1100

    Add validation setup for Employee

--- src/test/java/com/laurencetuchin/employeesystemapi/entities/EmployeeTest.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/entities/EmployeeTest.java
@@ -1,7 +1,20 @@
 package com.laurencetuchin.employeesystemapi.entities;
 
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeeTest {
 
+    private static Validator validator;
+
+    @BeforeTestClass
+    public static void setUp() {
+        ValidatorFactory validatorFactory = Validation.buildDefaultValidatorFactory();
+        validator = validatorFactory.getValidator();
+    }
 }
