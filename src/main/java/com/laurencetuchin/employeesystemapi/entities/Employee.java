42dbb01f861d81fbe86d84cb9efe905900a5fdb5
laurencetuchin <laurencetuchin@gmail.com>
2022-11-18 11:03:51 +1100

    Create regex email class to make solid for later

--- src/main/java/com/laurencetuchin/employeesystemapi/entities/Employee.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/entities/Employee.java
@@ -1,5 +1,6 @@
 package com.laurencetuchin.employeesystemapi.entities;
 
+import com.laurencetuchin.employeesystemapi.seedData.RegexEmailCompliant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +29,7 @@ public class Employee {
 
     @Column(name = "email")
     @Email(message = "Email must be valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+//    @Email(message = "Email must be valid", regexp = RegexEmailCompliant  );
     private String email;
 
     @Column(name = "employment_status")
