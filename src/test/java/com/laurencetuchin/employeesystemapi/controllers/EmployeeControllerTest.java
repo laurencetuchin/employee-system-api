b859c4295b4f1d07915422758c25c11f29791441
laurencetuchin <laurencetuchin@gmail.com>
2022-11-23 12:30:23 +1100

    Add in @AutoConfigureMockMvc and @ExtendWith on test class

--- src/test/java/com/laurencetuchin/employeesystemapi/controllers/EmployeeControllerTest.java
+++ src/test/java/com/laurencetuchin/employeesystemapi/controllers/EmployeeControllerTest.java
@@ -3,7 +3,10 @@ package com.laurencetuchin.employeesystemapi.controllers;
 import com.laurencetuchin.employeesystemapi.entities.Employee;
 import com.laurencetuchin.employeesystemapi.services.EmployeeService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -28,6 +31,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @WebMvcTest(EmployeeController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
 class EmployeeControllerTest {
 
 
