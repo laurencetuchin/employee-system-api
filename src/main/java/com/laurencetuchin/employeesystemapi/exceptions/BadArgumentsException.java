0e4be2884013da0bddfac654fe9b1d4c9f39fff5
laurencetuchin <laurencetuchin@gmail.com>
2022-12-01 16:06:11 +1100

    Add BadArgumentsException class for error handling

---
+++ src/main/java/com/laurencetuchin/employeesystemapi/exceptions/BadArgumentsException.java
@@ -0,0 +1,13 @@
+package com.laurencetuchin.employeesystemapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadArgumentsException extends RuntimeException {
+
+    public BadArgumentsException(String message) {
+        super(message);
+    }
+
+}
