79d15d62b72467bd79f9fe6655f43be40afa1f9d
laurencetuchin <laurencetuchin@gmail.com>
2022-12-01 16:03:23 +1100

    Add InternalException class for error handling

---
+++ src/main/java/com/laurencetuchin/employeesystemapi/exceptions/InternalException.java
@@ -0,0 +1,12 @@
+package com.laurencetuchin.employeesystemapi.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalException extends RuntimeException {
+
+    public InternalException(String message) {
+        super(message);
+    }
+}
