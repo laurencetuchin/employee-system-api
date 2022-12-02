95a383a49c89e77293f0ec5f8203a56f90c6761e
laurencetuchin <laurencetuchin@gmail.com>
2022-12-02 17:03:55 +1100

    Add primary key for Project entity

--- src/main/java/com/laurencetuchin/employeesystemapi/entities/Project.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/entities/Project.java
@@ -1,10 +1,14 @@
 package com.laurencetuchin.employeesystemapi.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Project {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String assignedEmployees;
