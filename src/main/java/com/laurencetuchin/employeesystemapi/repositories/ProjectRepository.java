1f0219e353940bdf16c8442b6065030938fee159
laurencetuchin <laurencetuchin@gmail.com>
2022-12-02 17:35:23 +1100

    Add repository finder query derivation methods

--- src/main/java/com/laurencetuchin/employeesystemapi/repositories/ProjectRepository.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/repositories/ProjectRepository.java
@@ -4,6 +4,15 @@ import com.laurencetuchin.employeesystemapi.entities.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project,Long> {
+
+    List<Project> findProjectByNameIgnoreCaseContains(String projectName);
+    List<Project> findProjectByAssignedEmployeeIgnoreCaseContains(String assignedEmployee);
+    List<Project> findProjectByStatusIgnoreCaseContains(String projectName);
+
+
+
 }
