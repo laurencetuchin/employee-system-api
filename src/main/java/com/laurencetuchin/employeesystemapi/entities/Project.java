d15894ecd72b8c76bf93b0131fcb479645aa5c04
laurencetuchin <laurencetuchin@gmail.com>
2022-12-02 17:05:32 +1100

    Add table names for fields

--- src/main/java/com/laurencetuchin/employeesystemapi/entities/Project.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/entities/Project.java
@@ -1,16 +1,15 @@
 package com.laurencetuchin.employeesystemapi.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Project {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name = "name")
     private String name;
+    @Column(name = "assignedEmployees")
     private String assignedEmployees;
 
 
