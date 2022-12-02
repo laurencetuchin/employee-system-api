61e2aa12c913b3858c7f72876ab2355674cfe8a6
laurencetuchin <laurencetuchin@gmail.com>
2022-12-02 19:35:14 +1100

    Add @Query for speed efficiency testing comparison

--- src/main/java/com/laurencetuchin/employeesystemapi/repositories/ProjectRepository.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/repositories/ProjectRepository.java
@@ -2,12 +2,17 @@ package com.laurencetuchin.employeesystemapi.repositories;
 
 import com.laurencetuchin.employeesystemapi.entities.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project,Long> {
+    @Query("select count(p) from Project p where p.assignedEmployees = ?1")
+    long countByAssignedEmployees(String assignedEmployees);
+    @Query("select p from Project p where p.status = ?1")
+    List<Project> findByStatus(Project.ProjectStatus status);
 
     List<Project> findProjectByNameIgnoreCaseContains(String projectName);
     List<Project> findProjectByAssignedEmployeeIgnoreCaseContains(String assignedEmployee);
