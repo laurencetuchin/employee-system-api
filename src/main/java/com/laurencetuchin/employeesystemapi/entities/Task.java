45e7032eb6fb99f2b04022a66a7e08adcfde4dbc
laurencetuchin <laurencetuchin@gmail.com>
2022-12-03 22:21:43 +1100

    Complete fields for Task entity

--- src/main/java/com/laurencetuchin/employeesystemapi/entities/Task.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/entities/Task.java
@@ -9,15 +9,92 @@ public class Task {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "name")
+    private String name;
+
     @Column(name = "description")
     private String description;
 
+    @Column(name = "status")
     private TaskStatus status;
 
+    @Column(name = "priority")
+    private TaskPriority priority;
+
+    @Column(name = "startDate")
     private LocalDateTime startDate;
 
+    @Column(name = "endDate")
     private LocalDateTime endDate;
 
+
+    public Task(String name, String description, TaskStatus status, TaskPriority priority, LocalDateTime startDate, LocalDateTime endDate) {
+        this.name = name;
+        this.description = description;
+        this.status = status;
+        this.priority = priority;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public TaskStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(TaskStatus status) {
+        this.status = status;
+    }
+
+    public TaskPriority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(TaskPriority priority) {
+        this.priority = priority;
+    }
+
+    public LocalDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDateTime startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDateTime getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDateTime endDate) {
+        this.endDate = endDate;
+    }
 
 
 }
