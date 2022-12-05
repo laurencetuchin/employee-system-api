59c501c3a88d1f8a8222a830dfec34bb335f7553
laurencetuchin <laurencetuchin@gmail.com>
2022-12-05 11:39:44 +1100

    Add @Query to Find Employee Tasks

--- src/main/java/com/laurencetuchin/employeesystemapi/repositories/EmployeeRepository.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/repositories/EmployeeRepository.java
@@ -1,7 +1,9 @@
 package com.laurencetuchin.employeesystemapi.repositories;
 
 import com.laurencetuchin.employeesystemapi.entities.Employee;
+import com.laurencetuchin.employeesystemapi.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -19,6 +21,10 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findEmployeeByNameAndRole(String partialName, String role);
 
     List<Employee> findEmployeeByNameOrRole(String partialName, String role);
+
+    @Query("select e from Employee e where e.employeeTasks = ?1")
+    List<Employee> findByEmployeeTasks(Task employeeTasks);
+
 
 
 
