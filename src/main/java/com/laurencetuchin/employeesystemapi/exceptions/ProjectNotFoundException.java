b95a49ec53848354b294607dc1cb0b26f8b5d3d0
laurencetuchin <laurencetuchin@gmail.com>
2022-12-16 20:19:44 +1100

    Add exception handler class for Projects

---
+++ src/main/java/com/laurencetuchin/employeesystemapi/exceptions/ProjectNotFoundException.java
@@ -0,0 +1,9 @@
+package com.laurencetuchin.employeesystemapi.exceptions;
+
+public class ProjectNotFoundException extends RuntimeException{
+
+    public ProjectNotFoundException(String message) {
+        super(message);
+    }
+
+}
