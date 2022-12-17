7bb8f4d5b2cb13df172a6db1bfb77098e8e785ff
laurencetuchin <laurencetuchin@gmail.com>
2022-12-18 10:52:31 +1100

    Update DtoMapper for Employee

--- src/main/java/com/laurencetuchin/employeesystemapi/dto/EmployeeDTO.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/dto/EmployeeDTO.java
@@ -3,18 +3,28 @@ package com.laurencetuchin.employeesystemapi.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.laurencetuchin.employeesystemapi.entities.EmploymentStatus;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class EmployeeDTO {
 
 
     private Long id;
 
+    @Size(min = 1, max = 10)
+    @NotNull @NotBlank
     private String name;
 
     @JsonProperty("UserRole")
     private String role;
 
+    @Email(message = "Email must be valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     private String email;
 
+    @JsonProperty("EmploymentStatus")
+    @NotNull
     private EmploymentStatus status;
 
     public void setId(Long id) {
--- src/main/java/com/laurencetuchin/employeesystemapi/mappers/EmployeeMapper.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/mappers/EmployeeMapper.java
@@ -2,10 +2,15 @@ package com.laurencetuchin.employeesystemapi.mappers;
 
 import com.laurencetuchin.employeesystemapi.dto.EmployeeDTO;
 import com.laurencetuchin.employeesystemapi.entities.Employee;
+import com.laurencetuchin.employeesystemapi.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+//@Service
 public class EmployeeMapper {
 
+    @Autowired
+    private EmployeeService employeeService;
 
     public EmployeeDTO toDto(Employee employee) {
         EmployeeDTO employeeDTO = new EmployeeDTO(employee.getId(), employee.getName(), employee.getRole(), employee.getEmail(), employee.getStatus());
@@ -18,12 +23,13 @@ public class EmployeeMapper {
         return employeeDTO;
     }
 
-    @Autowired
+//    @Autowired
     public Employee toEntity(EmployeeDTO employeeDto) {
-        Employee employee = new Employee(employeeDto.getName(), employeeDto.getRole());
+        Employee employee = new Employee(employeeDto.getName(), employeeDto.getRole(), employeeDto.getEmail(),employeeDto.getStatus());
         //  EmployeeDTO employeeDTO = new EmployeeDTO(employee.getId(), employee.getName(), employee.getRole());
      //   EmployeeDTO employeeDTO = new EmployeeDTO();
-
+        // save Employee?
+//        employeeService.save(employee);
      //   employeeDTO.setId(employee.getId());
      //   employeeDTO.setName(employee.getName());
     //    employeeDTO.setRole(employee.getRole());
