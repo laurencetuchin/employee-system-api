1efeb3eb2698d9d14fa5c4a15d4b4af03b635c59
laurencetuchin <laurencetuchin@gmail.com>
2022-12-19 10:40:10 +1100

    Update mapper to restrict employee fields in EmployeeDto

--- src/main/java/com/laurencetuchin/employeesystemapi/mappers/EmployeeMapper.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/mappers/EmployeeMapper.java
@@ -5,7 +5,6 @@ import com.laurencetuchin.employeesystemapi.entities.Employee;
 import com.laurencetuchin.employeesystemapi.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 
-//@Service
 public class EmployeeMapper {
 
     @Autowired
@@ -17,37 +16,19 @@ public class EmployeeMapper {
                 employee.getName(),
                 employee.getRole(),
                 employee.getEmail(),
-                employee.getStatus(),
-                employee.getDateOfBirth(),
-                employee.getCareerGoal()
+                employee.getStatus()
         );
-      //  EmployeeDTO employeeDTO = new EmployeeDTO();
-
-      //   employeeDTO.setId(employee.getId());
-      //  employeeDTO.setName(employee.getName());
-      //  employeeDTO.setRole(employee.getRole());
 
         return employeeDTO;
     }
 
-//    @Autowired
     public Employee toEntity(EmployeeDto employeeDto) {
         Employee employee = new Employee(
                 employeeDto.getName(),
                 employeeDto.getRole(),
                 employeeDto.getEmail(),
-                employeeDto.getStatus(),
-                employeeDto.getDateOfBirth(),
-                employeeDto.getCareerGoal()
-
+                employeeDto.getStatus()
         );
-        //  EmployeeDTO employeeDTO = new EmployeeDTO(employee.getId(), employee.getName(), employee.getRole());
-     //   EmployeeDTO employeeDTO = new EmployeeDTO();
-        // save Employee?
-//        employeeService.save(employee);
-     //   employeeDTO.setId(employee.getId());
-     //   employeeDTO.setName(employee.getName());
-    //    employeeDTO.setRole(employee.getRole());
 
         return employee;
     }
