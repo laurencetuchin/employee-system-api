4a0df0439d239b614d4dd0e9a05f99c06222aa87
laurencetuchin <laurencetuchin@gmail.com>
2022-12-19 10:23:39 +1100

    Update EmployeeDto to remove fields which should be hidden

--- src/main/java/com/laurencetuchin/employeesystemapi/dto/EmployeeDto.java
+++ src/main/java/com/laurencetuchin/employeesystemapi/dto/EmployeeDto.java
@@ -2,42 +2,21 @@ package com.laurencetuchin.employeesystemapi.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.laurencetuchin.employeesystemapi.entities.EmploymentStatus;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-import java.time.LocalDate;
 
 public class EmployeeDto {
 
 
     private Long id;
 
-
-    @Size(min = 1, max = 10)
-    @NotNull @NotBlank
     private String name;
 
-    @JsonProperty("UserRole")
+    @JsonProperty("User role")
     private String role;
 
-    @Email(message = "Email must be valid", regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
     private String email;
 
     @JsonProperty("EmploymentStatus")
-    @NotNull
     private EmploymentStatus status;
-
-//    @NotBlank
-    @DateTimeFormat
-    private LocalDate dateOfBirth;
-
-    @NotBlank
-    @Size(min = 1, max = 100)
-    private String careerGoal;
-
 
     public void setId(Long id) {
         this.id = id;
@@ -79,39 +58,16 @@ public class EmployeeDto {
         this.status = status;
     }
 
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getCareerGoal() {
-        return careerGoal;
-    }
-
-    public void setCareerGoal(String careerGoal) {
-        this.careerGoal = careerGoal;
-    }
-
-    public EmployeeDto(Long id, String name, String role, String email, EmploymentStatus status, LocalDate dateOfBirth, String careerGoal) {
+    public EmployeeDto(Long id, String name, String role, String email, EmploymentStatus status) {
         this.id = id;
         this.name = name;
         this.role = role;
         this.email = email;
         this.status = status;
-        this.dateOfBirth = dateOfBirth;
-        this.careerGoal = careerGoal;
     }
 
     @Override
     public String toString() {
-        return "EmployeeDTO{" +
-                "name='" + name + '\'' +
-                ", role='" + role + '\'' +
-                ", email='" + email + '\'' +
-                ", status=" + status +
-                '}';
+        return "EmployeeDTO{" + "name='" + name + '\'' + ", role='" + role + '\'' + ", email='" + email + '\'' + ", status=" + status + '}';
     }
 }
